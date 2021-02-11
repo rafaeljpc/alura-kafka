40bc87f9ef7ce4d51068dec9fa3ec3e735eca74e
Rafael Correia <rafael.correia@soulisto.com.br>
2021-02-11 18:37:18 -0300

    EmailService.kt consumer

--- services/ecommerce/src/main/kotlin/io/github/rafaeljpc/alura/kafka/ecommerce/EcommerceApplication.kt
+++ services/ecommerce/src/main/kotlin/io/github/rafaeljpc/alura/kafka/ecommerce/EcommerceApplication.kt
@@ -4,33 +4,37 @@ import mu.KotlinLogging
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
 import org.apache.kafka.common.serialization.StringSerializer
 import java.time.Instant
 import java.time.LocalDateTime
 import java.util.*
+import kotlin.random.Random
 import kotlin.reflect.jvm.jvmName
 
 private val log = KotlinLogging.logger {}
 
 fun main() {
     val producer = KafkaProducer<String, String>(properties())
+    val rnd = Random(System.currentTimeMillis())
+    val value = "${rnd.nextInt(1, 1000)}," +
+            "${rnd.nextInt(1, 1000)},${rnd.nextInt(1, 20) * 100}"
+    producer.send(ProducerRecord("ECOMMERCE_NEW_ORDER", value, value), producerCB).get()
 
-    val value = "1,2,3000"
+    val email = "Thank you for your order! We are processing your order"
+    producer.send(ProducerRecord("ECOMMERCE_SEND_EMAIL", email, email), producerCB).get()
+}
 
-    producer.send(
-        ProducerRecord("ECOMMERCE_NEW_ORDER", value, value)
-    ) { data, e ->
-        if (e != null) {
-            log.error("Error", e)
-            return@send
-        }
-
+private val producerCB = { data: RecordMetadata, e: Exception? ->
+    if (e != null) {
+        log.error("Error", e)
+    } else {
         log.info(
             "${data.topic()}:::${data.partition()}/${data.offset()}@${
                 LocalDateTime.ofInstant(Instant.ofEpochMilli(data.timestamp()), TimeZone.getDefault().toZoneId())
             }"
         )
-    }.get()
+    }
 }
 
 private fun properties(): Properties = hashMapOf(
--- services/ecommerce/src/main/kotlin/io/github/rafaeljpc/alura/kafka/ecommerce/EmailService.kt
+++ services/ecommerce/src/main/kotlin/io/github/rafaeljpc/alura/kafka/ecommerce/EmailService.kt
@@ -13,7 +13,7 @@ private val log = KotlinLogging.logger {}
 fun main() {
     val consumer = KafkaConsumer<String, String>(properties())
 
-    consumer.subscribe(listOf("ECOMMERCE_NEW_ORDER"))
+    consumer.subscribe(listOf("ECOMMERCE_SEND_EMAIL"))
 
     while (true) {
 
@@ -27,7 +27,7 @@ fun main() {
             log.info(
                 """
             ------------------------------------------
-            Processing New Order
+            Sending email
             key = ${it.key()}
             value = ${it.value()}
             partition = ${it.partition()}
@@ -35,10 +35,10 @@ fun main() {
         """.trimIndent()
             )
 
-            Thread.sleep(1000)
+            Thread.sleep(500)
             log.info(
                 """
-                Order processed
+                email sent
                 ------------------------------------------
                 """.trimIndent()
             )
@@ -50,5 +50,5 @@ private fun properties(): Properties = hashMapOf(
     ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
     ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.jvmName,
     ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.jvmName,
-    ConsumerConfig.GROUP_ID_CONFIG to "FraudDetectorService"
+    ConsumerConfig.GROUP_ID_CONFIG to "EmailService"
 ).toProperties()
