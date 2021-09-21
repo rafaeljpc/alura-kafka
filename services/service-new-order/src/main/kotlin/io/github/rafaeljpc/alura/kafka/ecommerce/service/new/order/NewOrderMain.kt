d02c72e580f127c03b2eba0f7b300ea490d478f6
Rafael Correia <rafael.correia@soulisto.com.br>
2021-09-21 09:54:29 -0300

    repeat message generation

--- services/service-new-order/src/main/kotlin/io/github/rafaeljpc/alura/kafka/ecommerce/service/new/order/NewOrderMain.kt
+++ services/service-new-order/src/main/kotlin/io/github/rafaeljpc/alura/kafka/ecommerce/service/new/order/NewOrderMain.kt
@@ -5,21 +5,28 @@ import io.github.rafaeljpc.alura.kafka.ecommerce.model.Order
 import java.math.BigDecimal
 import java.util.*
 
+private const val QTD = 30
+
 fun main() {
     val orderDispatcher = KafkaDispatcher<Order>()
     val emailDispatcher = KafkaDispatcher<String>()
 
-    val order = Order(
-        userId = UUID.randomUUID().toString(),
-        orderId = UUID.randomUUID().toString(),
-        amount = BigDecimal((1..5000).random())
-    )
+    repeat(QTD) {
+        val order = Order(
+            userId = UUID.randomUUID().toString(),
+            orderId = UUID.randomUUID().toString(),
+            amount = BigDecimal((1..5000).random())
+        )
 
-    orderDispatcher.send("ECOMMERCE_NEW_ORDER", order.userId, order)
-    emailDispatcher.send(
-        "ECOMMERCE_SEND_EMAIL", order.userId, """
-        Thank you for your order! We are processing your order
-        Order Number ${order.orderId}
-    """.trimIndent()
-    )
+        orderDispatcher.send("ECOMMERCE_NEW_ORDER", order.userId, order)
+        emailDispatcher.send(
+            "ECOMMERCE_SEND_EMAIL", order.userId,
+            """
+            Thank you for your order! We are processing your order
+            Order Number ${order.orderId}
+            """.trimIndent()
+        )
+    }
+
+
 }
